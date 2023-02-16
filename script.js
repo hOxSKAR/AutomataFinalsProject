8d50b61eebed50f38d2645486ccd2f0969b23c63
PCNIMIKA\mikia <virgo.hatena@gmail.com>
2023-02-16 15:13:13 +0800

    Added CFG and PDA buttons (not linked in JS yet also)

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function selectExpression() {
   
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.btn')) {
         var dropdowns = document.getElementsByClassName("expressions");
         var i;
     for (i = 0; i < dropdowns.length; i++) {
