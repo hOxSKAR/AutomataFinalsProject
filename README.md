90c285f6666f024905c5205ee83468676e1e3c29
Yatraak <50107372+Yahan-VDG@users.noreply.github.com>
2023-05-21 01:19:51 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
 # FinalsProject
 
 To Do List:
-- GUI
-- Expressions FA
-- Input logic code
-- Simulation animations
-- CFG and PDA
+- [ ] 80% done GUI
+- [ ] Expressions FA
+- [ ] CFG and PDA
+- [ ] Printing out code - Vea
+- [ ] Input logic code
+- [ ] Simulation animations
+
+
+* Papacheck pa kay miss yung FA, CFG, at PDA bago tayo magproceed sa coding.
+
+References used:
+- http://www.java2s.com/ref/javascript/javascript-dom-textarea-split-text-by-line.html (getting of input in textarea, split by newlines)
+- https://sebhastian.com/javascript-create-button/ (making button using JS)
+- https://www.geeksforgeeks.org/how-to-append-html-code-to-a-div-using-javascript/ (adding stuff in a div element using JS)
+- https://www.w3schools.com/tags/tag_textarea.asp (textarea reference)
+- https://codepen.io/chanthy/pen/WxQoVG (Arrow maker code for canvas)
+- https://www.w3schools.com/graphics/canvas_coordinates.asp (reference for drawing on canvas)
+- https://ivanzuzak.info/noam/webapps/fsm_simulator/ - automata reference for FA
+- https://www.valentinog.com/blog/html-table/ (making a table using JS - going to be used for printing out of inputted expressions, whether valid or not, and simulation button)
+- https://www.w3schools.com/jsref/jsref_push.asp (array push method - to be used in printing out)
+- https://stackoverflow.com/questions/15315315/how-do-i-add-a-button-to-a-td-using-js (button in table - to be used in printing out)
+- https://www.w3schools.com/js/js_objects.asp (this + array will be used for the printing out)
+- https://www.w3schools.com/js/js_switch.asp (the classic switch case reference cuz I forget stuff easily)
--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var canvas = document.getElementById("expressionCanvas");
+var ctx = canvas.getContext("2d");
+var expression;
+var eval;
+var validInner = document.getElementById("valid").innerHTML;
+
+// Textarea getting
+let userInput = document.getElementById("input");
+let lines = userInput.value.split("\n");
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function selectExpression() {
@@ -17,3 +27,61 @@ window.onclick = function (event) {
         }
     }
 };
+
+// Expression FAs
+function expression1() {
+    expression = "expression1";
+    
+    //canvas drawing of the FA
+    ctx.beginPath();
+    ctx.arc(30, 40, 15, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+function expression2() {
+    expression = "expression2";
+
+    //canvas drawing of the FA
+}
+
+// Expression Evaluation
+function eval() {
+    switch (expression) {
+        case "expression1":
+            //enter expression validation logic
+            break;
+        case "expression2":
+            //enter expression validation logic
+            break;
+        default:
+            alert("Please enter expressions to be evaluated.");
+    }
+}
+
+// Printing out of expressions and evaluation
+function printOut(expression, status) {
+    let final = [];
+
+    // Button maker
+    let btn = document.createElement("button");
+    btn.innerHTML = "Simulate?";
+    btn.onclick = simulate();
+    btn.class = "btn";
+
+    for (let i = 0; i < lines.length; i++) {
+        final.push({
+            Expression: "" + expression,
+            Evaluation: "" + status,
+            Simulate: ""
+        });
+    }
+
+    // Code creating table using JS objects...
+}
+
+// Simulation Animations
+function simulate() {
+    // Enter code
+}
+
+// Arrow maker - used in FA diagram
